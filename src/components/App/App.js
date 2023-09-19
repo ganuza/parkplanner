7e213111b20a52fecf8d5081622feb7f309117c0
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-19 14:06:03 -0600

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return(
+    <main className='App'>
+      <h1>ParkPlanner</h1>
+    </main>
+  )
+}
+
+export default App
