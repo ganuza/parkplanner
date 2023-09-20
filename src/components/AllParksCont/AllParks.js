fcf1f35b061df264bba5c148e714fd7d15c2bb9d
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-20 15:49:03 -0600

    Add function for mapping over parks to create props

--- src/components/AllParksCont/AllParks.js
+++ src/components/AllParksCont/AllParks.js
@@ -1,8 +1,20 @@
+import PropTypes from "prop-types"
 import './AllParks.css'
 
-function AllParks() {
+function AllParks( { allParksData }) {
+  const allParksCards = allParksData.map((park) => {
+    return (
+      <AllParksCard
+        key={park.id}
+        id={park.id}
+        img={park.image[0]}
+        // renderIndividualPark={renderIndividualPark}
+        />
+    )
+  })
   return (
     <section className="all-parks-cont">
+      <h2>Card</h2>
       <h2>Card</h2>
       <h2>Card</h2>
       <h2>Card</h2>
