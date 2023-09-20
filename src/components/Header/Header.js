12592c5722a3aaf0993b2523133e02470c722859
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-19 19:25:42 -0600

    Add Search component to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import Search from '../Search/Search'
 import './Header.css'
 
 function Header() {
   return (
     <div className='title-cont'>
       <h1>ParkPlanner</h1>
+      <Search />
     </div>
   )
 }
