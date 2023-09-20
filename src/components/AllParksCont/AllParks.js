57c7958c4437122821071f40f4a5e70c212cd59f
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-20 15:32:39 -0600

    Add component for AllParksCont

---
+++ src/components/AllParksCont/AllParks.js
@@ -0,0 +1,13 @@
+import './AllParks.css'
+
+function AllParks() {
+  return (
+    <section className="all-parks-cont">
+      <h2>Card</h2>
+      <h2>Card</h2>
+      <h2>Card</h2>
+    </section>
+  )
+}
+
+export default AllParks
