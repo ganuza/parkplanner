6dc8b9842642f891b4a91a768c70b00828634b84
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-22 18:13:18 -0600

    Add link to home on title in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import Search from '../Search/Search'
+import { Link } from 'react-router-dom'
+import npsLogo from '../../Assets/Arrowhead_3.png'
 import './Header.css'
 
 function Header() {
   return (
-    <div className='title-cont'>
-      <h1>ParkPlanner</h1>
-      <Search />
-    </div>
+    <header>
+      <div className='title-cont'>
+        <Link to={'/'}>
+            <h1>ParkPlanner</h1>
+        </Link>
+        <img className="nps-icon" src={npsLogo}
+        alt="National Park Service Icon"
+        />
+        <Search />
+      </div>
+    </header>
   )
 }
 
