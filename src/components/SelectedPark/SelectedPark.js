66033052940ad24350b0ea9a32ffaae17763ca08
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-23 13:28:58 -0600

    Add functionality for passing selected park data as props

--- src/components/SelectedPark/SelectedPark.js
+++ src/components/SelectedPark/SelectedPark.js
@@ -1,53 +1,59 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { getSelectedPark } from '../../apiCalls'
 import SelectedParkCard from '../SelectedParkCard/SelectedParkCard'
-import PropTypes from 'prop-types'
+import ErrorComponent from '../Error/Error'
 import './SelectedPark.css'
 
-const SelectedPark = ({allParksData}) => {
-  console.log('SelectedPark data: ', allParksData)
-  const { parkId } = useParams()
+const SelectedPark = () => {
+ 
+  const { parkCode } = useParams()
+  const [selectedPark, setSelectedPark] = useState([])
+  const [error, setError] = useState('')
 
-  const selectedParkDetails = allParksData.find((park) => {
-      return park.id === parkId
+  useEffect(() => {
+    getSelectedPark(parkCode)
+      .then((data) => {
+        console.log('selectedParkData: ', data.data[0])
+        setSelectedPark(data.data[0])
+      })
+      .catch((error) => {
+        setError('An error occurred while fetching park data.')
+      })
+  }, [parkCode])
+
+  if (error) {
+    return <ErrorComponent message={error} resetError={() => setError(null)} />
+  }
+
+  if (!selectedPark || !selectedPark.activities) {
+  return <div>Loading...</div>
+  }
+
+  const parkActivities = selectedPark.activities.map((activity) => {
+    return activity.name;
+  });
+  const image = selectedPark.images[1]?.url
+  const parkDescription = selectedPark.description
+  const activitiesList = parkActivities.map((activity, index) => {
+      return <li key={index}>{activity}</li>
     })
-  
-    console.log('selectedParkDetails: ', selectedParkDetails)
-  
-  const activities = []
-  const parkActivities = selectedParkDetails.activities.map((activity) => {
-    return activities.push(activity.name)
-  })
-  console.log('activities: ', activities)
-  
-  const image = selectedParkDetails.images[1]?.url
-  
-  console.log('image: ', image)
-  
-  const isImageAvailable = Boolean(image)
+  const parkDirections = selectedPark.directionsInfo
+  const parkEmail = selectedPark.contacts.emailAddresses[0].emailAddress
+  const parkPhone = selectedPark.contacts.phoneNumbers[0].phoneNumber
 
-
-  const parkDescription = selectedParkDetails.description
-  const activitiesList = activities.map((activity, index) => {
-    return <li key={index}>{activity}</li>
-  })
-  const parkDirections = selectedParkDetails.directionsInfo
   return (
     <div>
       <SelectedParkCard className="sel-park-card-cont"
-        
         image={image}
         parkDescription={parkDescription}
         activitiesList={activitiesList}
         parkDirections={parkDirections}
+        parkEmail={parkEmail}
+        parkPhone={parkPhone}
       />
-      
-
     </div>
   )
 }
 
 export default SelectedPark
-
-SelectedPark.propTypes = {
-  allParksData: PropTypes.array.isRequired
-}
