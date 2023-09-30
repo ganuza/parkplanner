78f8c15cb6bdca73c3708b65cee8c79eca9042ba
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-30 11:32:30 -0600

    Add error handling for non ok responses for getSelectedPark

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,7 +10,10 @@ export const getAllParksInfo = () => {
 
 export const getSelectedPark = (parkCode) => {
   return fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=eUYXl96Cb1zurRmYKjTUhznFQ23gnifeMJidB3rG`)
-    .then(response => {
-      return response.json()
+    .then(res => {
+      if (!res.ok) {
+        throw new Error ('Server Error.')
+      }
+      return res.json()
     })
 }
