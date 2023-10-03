106a6ad0f697395f4df84b346eba053786d9eb8e
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-03 12:13:46 -0600

    Add proptypes for AllParksData array

--- src/components/AllParks/AllParks.js
+++ src/components/AllParks/AllParks.js
@@ -38,6 +38,47 @@ function AllParks({ allParksData, searchTerm }) {
 export default AllParks
 
 AllParks.propTypes = {
-  allParksData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  allParksData: PropTypes.arrayOf(PropTypes.shape({
+    activities: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string
+    })),
+    addresses: PropTypes.arrayOf(PropTypes.shape({
+      city: PropTypes.string,
+      countryCode: PropTypes.string,
+      line1: PropTypes.string,
+      line2: PropTypes.string,
+      line3: PropTypes.string,
+      postalCode: PropTypes.string,
+      provinceTerritoryCode: PropTypes.string,
+      stateCode: PropTypes.string,
+      type: PropTypes.string,
+    })),
+    contacts: PropTypes.objectOf(PropTypes.array),
+    description: PropTypes.string,
+    designation: PropTypes.string,
+    directionsInfo: PropTypes.string,
+    directionsUrl: PropTypes.string,
+    entranceFees: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+    entrancePasses: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+    fees: PropTypes.array,
+    fullName: PropTypes.string,
+    id: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+    latLong: PropTypes.string,
+    latitude: PropTypes.string,
+    longitude: PropTypes.string,
+    name: PropTypes.string,
+    operatingHours: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.array,
+      PropTypes.object,
+    ]))),
+    parkCode: PropTypes.string,
+    states: PropTypes.string,
+    topics: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+    url: PropTypes.string,
+    weatherInfo: PropTypes.string,
+  })).isRequired,
   searchTerm: PropTypes.string.isRequired,
 }
